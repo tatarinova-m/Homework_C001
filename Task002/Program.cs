3c2aaf597891aa4e6d0ba6c15f4e4dcdd835080b
Maria <masha.tatarinova01@mail.ru>
2023-02-05 01:07:20 +0700

    changed

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -11,33 +11,30 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
 
 int max = number1;
-int count = 0;
 
-while (count < 4)
-{
-    if (number1 > number2)
+if (number1 > number2)
     {
-        max = number1;
+        if (number1 > number3)
+        {
+            max = number1;
+        }
+        else 
+        {
+            max = number3;
+        }
     }
 
-    else
+else
+{
+    if (number2 > number3)
     {
         max = number2;
     }
-
-
-    if (max > number3)
-    {
-        max = max;
-    }
-
     else
     {
         max = number3;
     }
-
-    count++;
 }
-
+    
 Console.WriteLine($"Максимальное значение = {max}");
 
